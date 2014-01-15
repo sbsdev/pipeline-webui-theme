2d67a359669052147a1be05858c29be6f0da4c7d
Bert Frees <bert.frees@sbs.ch>
2014-01-15 17:24:38 +0100

    Move the theme to its own project and Debianize

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  . ((compile-command . (format "cd %s && mvn clean package"
+                                (locate-dominating-file buffer-file-name "pom.xml")))))
+ ("pom.xml"
+  . ((nil . ((indent-tabs-mode . nil)
+             (nxml-child-indent . 2))))))
---
+++ src/main/resources/javascripts/theme.js
@@ -0,0 +1,9 @@
+// include jquery
+//var script = document.createElement('script');
+//script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
+//script.type = 'text/javascript';
+//document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function() {
+  $(".navbar-fixed-top").prepend("<a class='sbs-logo' title='SBS'  href='http://www.sbs.ch''>SBS</a>");
+});
